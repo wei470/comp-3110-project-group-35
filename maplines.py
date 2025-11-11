5ae8f4cf5f4f14a77b37b2d3e869b307ebda7154
hongyuan wei <wei47@uwindsor.ca>
2025-11-10 23:15:28 -0500

    init the file, open python -u

--- maplines.py
+++ maplines.py
@@ -1,1 +1,15 @@
-print("helloworld")
+import argparse
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("old_file")
+    ap.add_argument("new_file")
+    ap.add_argument("-o", "--output", default="mapping.json")
+    args = ap.parse_args()
+    
+    with open(args.old_file, encoding="utf-8", errors="ignore") as f:
+        old_lines = f.readlines()
+    with open(args.new_file, encoding="utf-8", errors="ignore") as f:
+        new_lines = f.readlines()
+
+    print("done")
